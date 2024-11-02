0dfcf1b23fa7933472bd61e090c728b84f27e2c8
lauraoliveri <lauraoliveri2003@gmail.com>
2024-11-02 17:52:38 +0100

    Create new migration to add foreign key to table types

---
+++ database/migrations/2024_11_02_164117_add_foreign_key_to_projects.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+         $table->foreign('type_id')
+            ->references('id')
+            ->on('types')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+        
+            // per eliminare solo la colonna della foreign key in caso di rollback
+            $table->dropForeign(['type_id']);
+
+        });
+    }
+};
